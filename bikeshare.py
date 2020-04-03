c7dd692a6bf3ff375273248af5b41f2149c14a81
Kavi <kavi@Ls-iMac-3.hsd1.ca.comcast.net>
2020-04-02 23:24:18 -0700

    Refactored on the most common month of the year statistics

--- bikeshare.py
+++ bikeshare.py
@@ -98,7 +98,7 @@ def time_stats(df):
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
 
-    # TO DO: display the most common month
+    # TO DO: display the most common month of the year
     common_month = df['month'].mode()[0]
     print("The most common month is:", common_month)
     
